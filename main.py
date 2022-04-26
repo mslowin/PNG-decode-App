b4f3b4c377af14fbcb3430f812095cc2ef78d32d
Michał Słowiński <62020662+mslowin@users.noreply.github.com>
2022-04-26 23:23:58 +0200

    Initial commit
    Added basic decoding of png files

---
+++ main.py
@@ -0,0 +1,66 @@
+# import numpy as np
+# import sys
+from PIL import Image
+import png_operations as png
+
+IHDR_hex = '0x490x480x440x52'
+PLTE_hex = '0x500x4c0x540x45'
+IDAT_hex = '0x490x440x410x54'
+IEND_hex = '0x490x450x4e0x44'
+
+
+image = Image.open('.\\PNG_images\\icon.png')
+# image.show()
+
+file_path = '.\\PNG_images\\icon.png'
+# file_path = '..\\png-is-my-favourite-file-type-master\\png_files\\type_3.png'
+
+with open(file_path, 'rb') as file:
+    content = [hex(a) for a in file.read()]
+
+# png.print_png_data(content)
+
+
+i = 0
+x = 0
+
+idat_start = []
+idat_end = []
+
+for i in range(len(content)-3):
+    # print((str(content[i]) + str(content[i + 1]) + str(content[i + 2]) + str(content[i + 3])))
+    if (str(content[i]) + str(content[i + 1]) + str(content[i + 2]) + str(content[i + 3])) == IHDR_hex:
+        print()
+        # png.show_ihdr_contents(content)
+        ihdr_length = png.print_ihdr_data(content, i)
+        ihdr_start = 8
+        ihdr_end = ihdr_start + 4 + 4 + ihdr_length + 4
+        print()
+    if (str(content[i]) + str(content[i + 1]) + str(content[i + 2]) + str(content[i + 3])) == PLTE_hex:
+        print()
+        plte_length = png.print_plte_data(content, i)
+        plte_start = i - 4
+        plte_end = plte_start + 3 + 4 + plte_length + 4
+        print()
+    if (str(content[i]) + str(content[i + 1]) + str(content[i + 2]) + str(content[i + 3])) == IDAT_hex:
+        print()
+        idat_length = png.print_idat_data(content, i)
+        idat_start.append(i - 4)
+        idat_end.append(idat_start[x] + 3 + 4 + idat_length + 4)
+        print()
+        x += 1
+    if (str(content[i]) + str(content[i + 1]) + str(content[i + 2]) + str(content[i + 3])) == IEND_hex:
+        print()
+        iend_length = png.print_iend_data(content, i)
+        iend_start = i - 4
+        iend_end = iend_start + 3 + 4 + iend_length + 4
+        print()
+
+
+# png_operations.print_png_data(content)
+
+# a = np.fft.fft2([[1, 2], [3, 4]])
+# print()
+# print(a)
+# print(np.fft.fftshift(a))
+# print(np.abs(a))
---
+++ png_operations.py
@@ -0,0 +1,113 @@
+def print_png_data(content):
+    j = 0
+    for i in range(len(content)):
+        print(content[i], end=" ")
+        j += 1
+        if j == 16:
+            j = 0
+            print()
+
+
+def image_width(content):
+    image_width_hex = (str(content[16])[2:]) + (str(content[17])[2:]) + (str(content[18])[2:]) + (str(content[19])[2:])
+    print('Image width:\t', end=" "), print(int(image_width_hex, 16))
+
+
+def image_height(content):
+    image_height_hex = (str(content[20])[2:]) + (str(content[21])[2:]) + (str(content[22])[2:]) + (str(content[23])[2:])
+    print('Image height:\t', end=" "), print(int(image_height_hex, 16))
+
+
+def image_bit_depth(content):
+    image_bit_depth_hex = str(content[24])[2:]
+    print('Image bit depth:\t', end=" "), print(int(image_bit_depth_hex, 16))
+
+
+def image_colour_type(content):
+    image_colour_type_hex = str(content[25])[2:]
+    print('Image colour type:\t', end=" "), print(int(image_colour_type_hex, 16))
+
+
+def image_compression_method(content):
+    image_compression_method_hex = str(content[26])[2:]
+    print('Image compression method:\t', end=" "), print(int(image_compression_method_hex, 16))
+
+
+def image_filter_method(content):
+    image_filter_method_hex = str(content[27])[2:]
+    print('Image filter method:\t', end=" "), print(int(image_filter_method_hex, 16))
+
+
+def image_interlace_method(content):
+    image_interlace_method_hex = str(content[28])[2:]
+    print('Image interlace method:\t', end=" "), print(int(image_interlace_method_hex, 16))
+
+
+def show_ihdr_contents(content):
+    image_width(content)
+    image_height(content)
+    image_bit_depth(content)
+    image_colour_type(content)
+    image_compression_method(content)
+    image_filter_method(content)
+    image_interlace_method(content)
+
+
+def print_ihdr_data(content, i):
+    j = 0
+    ihdr_length = content[i - 4][2:] + content[i - 3][2:] + content[i - 2][2:] + content[i - 1][2:]
+    ihdr_length = int(ihdr_length, 16)
+    print('IHDR chunk length: ', end=" "), print(ihdr_length, end=" "), print(' bytes')
+    for a in range(ihdr_length + 4 + 4 + 4):  # metadata_length + 4 bytes length + 4 bytes name + 4 bytes CRC
+        print(content[i - 4], end=" ")
+        i += 1
+        j += 1
+        if j == 16:
+            j = 0
+            print()
+    return ihdr_length
+
+
+def print_idat_data(content, i):
+    j = 0
+    idat_length = content[i - 4][2:] + content[i - 3][2:] + content[i - 2][2:] + content[i - 1][2:]
+    idat_length = int(idat_length, 16)
+    print('IDAT chunk length: ', end=" "), print(idat_length, end=" "), print(' bytes')
+    for a in range(idat_length + 4 + 4 + 4):  # metadata_length + 4 bytes length + 4 bytes name + 4 bytes CRC
+        print(content[i-4], end=" ")
+        i += 1
+        j += 1
+        if j == 16:
+            j = 0
+            print()
+    return idat_length
+
+
+def print_plte_data(content, i):
+    j = 0
+    plte_length = content[i - 4][2:] + content[i - 3][2:] + content[i - 2][2:] + content[i - 1][2:]
+    plte_length = int(plte_length, 16)
+    print('PLTE chunk length: ', end=" "), print(plte_length, end=" "), print(' bytes')
+    for a in range(plte_length + 4 + 4 + 4):  # metadata_length + 4 bytes length + 4 bytes name + 4 bytes CRC
+        print(content[i-4], end=" ")
+        i += 1
+        j += 1
+        if j == 16:
+            j = 0
+            print()
+    return plte_length
+
+
+def print_iend_data(content, i):
+    j = 0
+    iend_length = content[i - 4][2:] + content[i - 3][2:] + content[i - 2][2:] + content[i - 1][2:]
+    iend_length = int(iend_length, 16)
+    print('IEND chunk length: ', end=" "), print(iend_length, end=" "), print(' bytes')
+    for a in range(iend_length + 4 + 4 + 4):  # metadata_length + 4 bytes length + 4 bytes name + 4 bytes CRC
+        print(content[i-4], end=" ")
+        i += 1
+        j += 1
+        if j == 16:
+            j = 0
+            print()
+    return iend_length
